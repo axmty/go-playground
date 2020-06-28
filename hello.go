569767af1d1efdfa263cf8fd00b20e7d08b38127
axmty <alexandre.metgy@gmail.com>
2020-06-29 00:14:31 +0200

    Use the remote go-cmp/cmp package.

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/axmty/go-playground/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello world!"))
+	fmt.Println(cmp.Diff("Hello world", "Hello go"))
 }
