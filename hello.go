08f954439de0e78f24ac76a77776099cce567eb7
axmty <alexandre.metgy@gmail.com>
2020-07-07 01:24:31 +0200

    Play with goroutines.

--- hello.go
+++ hello.go
@@ -507,6 +507,51 @@ func playRot13Reader() {
 	fmt.Println()
 }
 
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func fibo(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		fmt.Printf("Fibo step %v\n", i+1)
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func playGoroutines() {
+	go say("world")
+	say("hello")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
+
+	c = make(chan int)
+	go fibo(10, c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello world!"))
 	fmt.Println(cmp.Diff("Hello world", "Hello go"))
@@ -595,4 +640,5 @@ func main() {
 
 	playReaders()
 	playRot13Reader()
+	playGoroutines()
 }
