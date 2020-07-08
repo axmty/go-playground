96901ed0d60581f7f734c17e77dd797e98e78d8f
axmty <alexandre.metgy@gmail.com>
2020-07-08 18:16:25 +0200

    More with goroutines.

--- hello.go
+++ hello.go
@@ -532,6 +532,36 @@ func fibo(n int, c chan int) {
 	close(c)
 }
 
+func fibo2Chans(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func defaultSelect() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
 func playGoroutines() {
 	go say("world")
 	say("hello")
@@ -550,6 +580,22 @@ func playGoroutines() {
 	for i := range c {
 		fmt.Println(i)
 	}
+
+	c = make(chan int)
+	quit := make(chan int)
+
+	fmt.Println("start fibo with 2 channels")
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibo2Chans(c, quit)
+
+	defaultSelect()
+
+	fmt.Println("end method playGoroutines")
 }
 
 func main() {
