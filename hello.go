9f51f206dc203db485325d5851705cca89f6c9f4
axmty <alexandre.metgy@gmail.com>
2020-07-08 19:35:01 +0200

    Implement Same for BT equivalence.

--- hello.go
+++ hello.go
@@ -602,13 +602,38 @@ func playGoroutines() {
 // Walk walks the tree t sending all values from
 // the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	if t.Left != nil {
-		Walk(t.Left, ch)
+	var walker func(t *tree.Tree)
+	walker = func(t *tree.Tree) {
+		if t == nil {
+			return
+		}
+		walker(t.Left)
+		ch <- t.Value
+		walker(t.Right)
 	}
-	ch <- t.Value
-	if t.Right != nil {
-		Walk(t.Right, ch)
+	walker(t)
+	close(ch)
+}
+
+// Same determines whether the trees t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		if v1 != v2 || ok1 != ok2 {
+			return false
+		}
+
+		if !ok1 {
+			break
+		}
 	}
+	return true
 }
 
 func equivBinTrees() {
@@ -623,6 +648,9 @@ func equivBinTrees() {
 		fmt.Printf("%d", n)
 	}
 	fmt.Println()
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
 
 func main() {
